c2d8e4723b10b4ac8bd594f87d42913c80c1e39a
alex <ajcorbett1305@gmail.com>
2026-03-13 22:16:07 +0000

    Updating hypr, adding rofi and nvim and fixing an issue with waybar

---
+++ .config/nvim/lua/plugins/colourscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "moonfly",
+    },
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+}
